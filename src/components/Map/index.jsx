404f44e168f8ee188d897063c7ffb17c1f02f239
Ovidio Matiazi Neto <netomatiazi@hotmail.com>
2022-02-23 19:14:18 -0300

    pequena modificação

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
-import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
+import {GoogleApiWrapper, Marker} from 'google-maps-react';
 
 import { Maps } from './style.js'
 
